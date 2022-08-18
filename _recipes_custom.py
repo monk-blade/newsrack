0a42200d39c73f5aef5e7a92f0ff7055986d3dc8
monk-blade <arpanchavdaeng@gmail.com>
2022-08-18 10:32:53 +0000

    fork test check

--- _recipes_custom.py
+++ _recipes_custom.py
@@ -1,18 +1,83 @@
 from typing import List
 
-from _recipe_utils import Recipe
+from _recipe_utils import Recipe, onlyon_days, onlyat_hours, onlyon_weekdays
 
 # Define the categories display order, optional
-categories_sort: List[str] = []
+categories_sort: List[str] = ["News", "Magazines", "Books"]
 
 # Define your custom recipes list here
 # Example: https://github.com/ping/newsrack-fork-test/blob/custom/_recipes_custom.py
 
 recipes: List[Recipe] = [
-    # Recipe(
-    #     recipe="example",
-    #     slug="example",
-    #     src_ext="epub",
-    #     category="example",
-    # ),
+    Recipe(
+        recipe="atlantic-magazine",
+        slug="atlantic-magazine",
+        src_ext="mobi",
+        target_ext=["epub"],
+        overwrite_cover=False,
+        category="Magazines",
+        enable_on=onlyon_weekdays([0, 1, 2, 3, 4], -4)
+        and onlyon_days(list(range(32 - 14, 32)), -4),
+    ),
+    Recipe(
+        recipe="thediplomat",
+        name="The Diplomat",
+        slug="the-diplomat",
+        src_ext="mobi",
+        target_ext=["epub"],
+        category="Magazines",
+        enable_on=onlyon_weekdays([0, 1, 2, 3, 4, 5], 5.5),
+    ),
+    Recipe(
+        recipe="economist",
+        slug="economist",
+        src_ext="mobi",
+        target_ext=["epub"],
+        overwrite_cover=False,
+        category="Magazines",
+        timeout=240,
+    ),
+    Recipe(
+        recipe="hbr",
+        slug="hbr",
+        src_ext="mobi",
+        target_ext=["epub"],
+        overwrite_cover=False,
+        category="Magazines",
+        enable_on=onlyon_days(list(range(1, 1 + 3)) + list(range(32 - 14, 32)), -5),
+    ),
+    Recipe(
+        recipe="mit-tech-review",
+        slug="mit-tech-review-feed",
+        src_ext="mobi",
+        target_ext=["epub"],
+        category="Magazines",
+        enable_on=onlyon_weekdays([0, 1, 2, 3, 4, 5], -4),
+    ),
+    Recipe(
+        recipe="nature",
+        slug="nature",
+        src_ext="mobi",
+        target_ext=["epub"],
+        category="Magazines",
+        overwrite_cover=False,
+        enable_on=onlyon_weekdays([2, 3, 4], 0),
+    ),
+    Recipe(
+        recipe="scientific-american",
+        slug="scientific-american",
+        src_ext="mobi",
+        target_ext=["epub"],
+        category="Magazines",
+        overwrite_cover=False,
+        enable_on=onlyon_days(list(range(15, 31)), -5),  # middle of the month?
+    ),
+    Recipe(
+        recipe="wired",
+        slug="wired",
+        src_ext="mobi",
+        target_ext=["epub"],
+        overwrite_cover=True,
+        category="Magazines",
+    ),
 ]

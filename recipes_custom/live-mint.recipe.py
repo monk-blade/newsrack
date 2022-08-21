d6b7497a5c6cc4ad4601167c31c38635764741d0
monk-blade <arpanchavdaeng@gmail.com>
2022-08-21 17:55:50 +0000

    Update Live Mint(fix wsj premium article)

--- recipes_custom/live-mint.recipe.py
+++ recipes_custom/live-mint.recipe.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env  python
-
+import json
+import re
 from calibre.web.feeds.news import BasicNewsRecipe, classes
 from datetime import date
 
@@ -22,10 +23,14 @@ class LiveMint(BasicNewsRecipe):
     compress_news_images_auto_size = 10
     scale_news_images = (800, 800)
     remove_attributes = ['style', 'height', 'width']
+    remove_empty_feeds =  True
     extra_css = """
         .highlights{font-style: italic; font-size: medium; font-weight: bold; margin: 0;background: #eee; padding: 1em; border-radius: 0.5em; list-style:none}
         picture+div{font-size: small;display: block;font-weight: bold;margin:0.4em;}
         span.author::before{content: " - ";}
+        #img-cap {font-size:small; text-align:center;}
+        #auth-info {font-size:small; text-align:center;}
+        .summary{font-style:italic; color:#404040;}
         .articleInfo{display: inline-block; padding:0.4em; color:gray; font-size:small;}
     """
     if is_saturday:
@@ -55,11 +60,13 @@ class LiveMint(BasicNewsRecipe):
             dict(name='h1'),
             dict(name='picture'),
             dict(name='figcaption'),
+            dict(name='figure', attrs={'data-vars-mediatype':'image'}),
             classes('articleInfo FirstEle summary highlights paywall'),
         ]
         remove_tags = [
             classes(
                 'trendingSimilarHeight moreNews mobAppDownload label msgError msgOk exclusive disclamerText outsideSso disqus-comment-count moreAbout'
+                ' socialHolder imgbig disclamerText disqus-comment-count taboolaHeight'
             ),
             dict(name='p', attrs={'class':'summary'})
         ]
@@ -81,7 +88,19 @@ class LiveMint(BasicNewsRecipe):
             ('Budget', 'https://www.livemint.com/rss/budget'),
             ('Elections', 'https://www.livemint.com/rss/elections'),
         ]
-
+    def preprocess_raw_html(self, raw, *a):
+        if '<script>var wsjFlag=true;</script>' in raw:
+            m = re.search(r'type="application/ld\+json">[^<]+?"@type": "NewsArticle"', raw)
+            raw1 = raw[m.start():]
+            raw1 = raw1.split('>', 1)[1].strip()
+            data = json.JSONDecoder().raw_decode(raw1)[0]
+            value = data['hasPart']['value']
+            body = data['articleBody'] + '</p> <p>' + re.sub(r'([a-z]\.|[0-9]\.)([A-Z])', r'\1 <p> \2', value)
+            body = '<div class="FirstEle"> <p>' +  body  + '</p> </div>'
+            raw = re.sub(r'<div class="FirstEle">([^}]*)</div>', body, raw)
+            return raw
+        else:
+            return raw
     def preprocess_html(self, soup):
         for img in soup.findAll('img', attrs={'data-src': True}):
             img['src'] = img['data-src']
